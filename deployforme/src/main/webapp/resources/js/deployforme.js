94a8973ca6b4d6ca6f4762cf85f4ceaa333ecdd4
macedoj <1633903+macedoj@users.noreply.github.com>
2017-10-25 13:29:11 -0200

    Js - Try to improve the JS code by Ebert recommendations

--- deployforme/src/main/webapp/resources/js/deployforme.js
+++ deployforme/src/main/webapp/resources/js/deployforme.js
@@ -1,28 +1,29 @@
 /*
+ * The MIT License
  * 
- The MIT License
- 
- Copyright 2016, Juliano Macedo.
- See LICENSE file for details.
- 
+ * Copyright 2016, Juliano Macedo.
+ * See LICENSE file for details.
  */
 
 /* 
- Created on : Aug 10, 2016, 10:34:54 PM
- Author     : Juliano Macedo
+ * Created on : Aug 10, 2016, 10:34:54 PM
+ * Author     : Juliano Macedo
  */
 
 
+/*
+ * global  $:false 
+ */ 
 $(document).ready(function () {
-    $("#subscribe").hide();
-
-    $("#btnshowsubscribe").click(function () {
-        $("#subscribe").slideToggle(1000);
+    $('subscribe').hide();
+    
+    $('#btnshowsubscribe').click(function () {
+      $('#subscribe').slideToggle(1000);
     });
 
     window.paceOptions = {
-        restartOnPushState: true,
-        startOnPageLoad: true,
-        ajax: true
+      restartOnPushState: true,
+      startOnPageLoad: true,
+      ajax: true
     };
 });
